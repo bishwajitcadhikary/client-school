7700e2c961684541c3632bc30c503c5eca7c2343
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-10-25 20:06:44 +0600

    build js

--- public/build/assets/Index.377e1514.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./app.886b2409.js";const n={name:"Index"};function t(r,o,s,c,_,a){return null}const d=e(n,[["render",t]]);export{d as default};
---
+++ public/build/assets/Index.bb9ffbd1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.113290fe.js";const n={name:"Index"};function t(r,o,s,c,_,a){return null}const d=e(n,[["render",t]]);export{d as default};
