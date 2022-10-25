f79eb96e7427cbe0851a0c2ded646425cdfb97c3
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-10-26 03:21:25 +0600

    build vue

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use Inertia\Inertia;
+use Symfony\Component\Process\Process;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +12,15 @@ Route::get('/', function () {
 
 Route::get('test', function (){
     return \App\Models\Setting::getSetting('date_format');
+});
+
+Route::get('install', function () {
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+
+    $process = new Process(['yarn build']);
+
+    $process->run();
 });
 
 require __DIR__.'/admin.php';
