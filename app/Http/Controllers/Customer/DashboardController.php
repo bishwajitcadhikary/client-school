5e1260ea9370ab4598a4955590e95a12afcb772a
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-11-04 00:13:16 +0600

    security

--- app/Http/Controllers/Customer/DashboardController.php
+++ app/Http/Controllers/Customer/DashboardController.php
@@ -13,7 +13,10 @@ class DashboardController extends Controller
     public function index()
     {
         $orders = PlanOrder::with(['gateway', 'plan'])->whereCustomerId(auth()->user()->id)->latest()->paginate();
-        $schools = School::whereCustomerId(auth()->id())->latest()->paginate();
+        $schools = School::whereCustomerId(auth()->id())
+            ->select(['id', 'name', 'domain', 'created_at', 'is_active', 'database_created'])
+            ->latest()
+            ->paginate();
 
         return Inertia::render('Customer/Dashboard/Index', [
             'orders' => $orders,
