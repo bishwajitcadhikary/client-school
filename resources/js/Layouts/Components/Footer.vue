152ee98f3f0d8c3c6e3ec6e6602d7627487ba02f
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-11-24 18:02:50 +0600

    Remove Developer name

--- resources/js/Layouts/Components/Footer.vue
+++ resources/js/Layouts/Components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-100 h-100 d-flex align-center justify-space-between">
     <!-- 👉 Footer: left content -->
-    <span class="d-flex align-center">
+<!--    <span class="d-flex align-center">
       &copy;
       {{ new Date().getFullYear() }}
       Developed With
@@ -17,7 +17,7 @@
         rel="noopener noreferrer"
         class="text-primary ms-1"
       >Bishwajit Adhikary</a>
-    </span>
+    </span>-->
     <!-- 👉 Footer: right content -->
     <span class="d-md-flex gap-x-4 text-primary d-none">
       <a
